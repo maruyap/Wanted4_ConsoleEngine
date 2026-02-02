473acadda854279e747a3b7e7d685cb426395f44
maruyap <oymin10@gmail.com>
2026-02-02 15:09:18 +0900

    소코반 게임 로직 구현
    - 플레이어 이동 검증 및 점수 처리 로직 구현
    - Stage2/3 텍스트 애셋 추가

--- Game/Level/SokobanLevel.h
+++ Game/Level/SokobanLevel.h
@@ -12,6 +12,8 @@ class SokobanLevel
 public:
 	SokobanLevel();
 
+	// 이벤트 함수 오버라이드.
+	virtual void Draw() override;
 private:
 
 	// 게임에서 사용할 맵을 로드하는 함수.
@@ -21,4 +23,14 @@ private:
 	virtual bool CanMove(
 		const Wanted::Vector2& playerPosition, 
 		const Wanted::Vector2& nextPosition) override;
+
+	// 게임 클리어 확인 함수.
+	bool CheckGameClear();
+
+private:
+	// 획득해야하는 목표 점수.
+	int targetScore = 0;
+
+	// 게임 클리어 여부를 알려주는 변수.
+	bool isGameClear = false;
 };
